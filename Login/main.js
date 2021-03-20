61a0cdc464e6b10fa7db18937626be81b3e1ccde
hsbatta <hsbatta@ncsu.edu>
2021-03-20 17:31:28 -0400

    Login Page

---
+++ Login/main.js
@@ -0,0 +1,24 @@
+    // let colorSchemeQueryList = window.matchMedia(
+    //   "(prefers-color-scheme: dark)"
+    // );
+
+    // const setColorScheme = e => {
+    //   if (e.matches) {
+    //     this.mode = "dark";
+    //     let container = document.getElementById("container");
+    //     console.log(container);
+    //     container.style.background = "#3d3d3d";
+    //     container.style.color = "#E8E8E8 ";
+    //     console.log(e);
+
+    //   } else {
+    //     this.mode = "light";
+    //     let container = document.getElementById("container");
+    //     console.log(container);
+    //     container.style.background = "#fff";
+    //     container.style.color = "#222 ";
+    //     console.log("Light mode");
+    //   }
+    // };
+    // setColorScheme(colorSchemeQueryList);
+    // colorSchemeQueryList.addListener(setColorScheme);
