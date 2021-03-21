9309e4dff294e536b2dcbe88cc3b16e80e7ced25
Srinath Vasudevan <svasude5@ncsu.edu>
2021-03-21 02:57:44 -0400

    did some testing with the login page and psot request. still unsure of if it works as intended

--- Login/index.php
+++ Login/index.php
@@ -8,22 +8,36 @@
 <title>Login Form Design</title>
     <link rel="stylesheet" href="style.css">
     <script type="text/javascript">
-        function myFunction() {
+        function login() {
             <?php
             $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"http://packhacks2021.srinath.tech/api/registernew");
+        curl_setopt($ch, CURLOPT_URL,"http://packhacks2021.srinath.tech/api/login");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,
-                "username=${_POST['username']}&password=${_POST['password']}&firstName=User&lastName=Name"
+                "username=${_POST['username']}&password=${_POST['password']}"
                 );
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
         // receive server response ...
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
+        $jsonOutput = json_decode($server_output, true);
+        if ($jsonOutput['status'] == 1) {
+            ?>
+            //Placeholder url to be navigated to on succesful authentication
+            //Javscript code
+            window.location.href = 'http://packhacks2021.srinath.tech/api/getusers';
+            <?php
+        } else {
+            ?>
+            alert("<?php echo $json_output["Error"] ?>");
+            <?php
+        }
         curl_close ($ch);
             ?>
+
         }
-        function login() {
+
+        function signup() {
             <?php
             $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,"http://packhacks2021.srinath.tech/api/registernew");
@@ -56,8 +70,8 @@
                         <label for="">Password</label>
                         <input type="password" class="form-control" name="password" required>
                     </div>
-                    <button id="post-btn" onclick="login()">Login</button>
-                    <button id="post-btn" onclick="myFunction()">Sign Up</button>
+                    <button id="post-btn" onclick="login">Login</button>
+                    <button id="post-btn2" onclick="signup">Sign Up</button>
                 </form>
 
             </div>    
